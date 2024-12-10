51702396fccaa93be4d62ee2a196078ed438e8cf
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-10 16:07:02 -0300

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "@components/layout/Layout";
+import { Layout } from "./components/layout/Layout";
 import { ReactNode } from "react";
 
 function App({ children }: { children: ReactNode }) {

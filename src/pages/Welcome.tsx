76c1e475e4eb467295da40a2a64d7bbc2202f557
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-11 21:41:58 -0300

    font changed && responsive quiz view

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -12,8 +12,8 @@ export const Welcome = ({ handleStartQuiz }: Props) => {
             <section className="welcome-window">
                 <div className="primary-content">
                     <div>
-                        <h1>How much you know about <span>Batman</span>?</h1>
-                        <h2>Test your knowledge of the Dark Knight's universe with this quiz!</h2>
+                        <span>How much you know about <span>Batman</span>?</span>
+                        <span>Test your knowledge of the Dark Knight's universe with this quiz!</span>
                     </div>
                     <div>
                         <Button onClick={handleStartQuiz}>Start Quiz</Button>

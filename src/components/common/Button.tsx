bdb5ef57b8258bfa525ac275cfbe25ecd071ca5d
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-23 11:30:29 -0300

    feat(button): add button disabled mode

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,15 +5,24 @@ type Color = `#${string}`;
 
 interface Params {
     children: ReactNode;
-    width?: string
+    disabled?: boolean;
+    width?: string;
     color?: Color;
     onClick?: () => void;
 }
 
-export const Button = ({ children, width="fit-content" , color = "#f1f1f1", onClick }: Params) => {
+export const Button = ({ children, disabled = false, width = "fit-content", color = "#f1f1f1", onClick }: Params) => {
     return (
-        <button type="button" style={{ color, width }} className={style["button"]} onClick={onClick ?? undefined}>
-            {children}
-        </button>
+        <>
+            {
+                disabled
+                    ? (<button disabled type="button" style={{ color, width }} className={style["button"]} onClick={onClick ?? undefined}>
+                        {children}
+                    </button>)
+                    : (<button type="button" style={{ color, width }} className={style["button"]} onClick={onClick ?? undefined}>
+                        {children}
+                    </button>)
+            }
+        </>
     );
 }

38eb4f1625128619db793ea55de3ca7a3ee6c011
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-01-14 17:11:21 -0300

    chore: add vie plugin for svg rendering

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-plugin-tsconfig-paths';
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
-  resolve: {
+  plugins: [
+    react(), 
+    tsConfigPaths(),
+    svgr(
+      {
+        svgrOptions: {
+          icon: true, // Usa el viewBox para hacer el SVG escalable
+          expandProps: 'end', // Expande las props para permitir width, height, fill, etc.
+        },
+      }
+    )],  resolve: {
     alias: {
       '@': "/src"
     },
     extensions: ['.ts', '.tsx', '.js', '.json']
-  }
+  },  
 })
